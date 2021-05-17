e3e72199f7ffac7b5056a9d0c9ae9db59fd8a2ad
Ochodus <mario1470@naver.com>
2021-05-18 02:59:23 +0900

    Modify code to use queue instead of array

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 hrrn : hrrn.c
-	gcc -o hrrn hrrn.c
+	gcc -g -o hrrn hrrn.c
 edf : edf.c
 	gcc -o edf edf.c
+clean : 
+	rm *.o hrrn
--- hrrn.c
+++ hrrn.c
@@ -1,61 +1,104 @@
 //hrrn.c
 
 #include <stdio.h>
+#include <stdlib.h>
 
-struct process {
+typedef struct Process {
 	char name;
 	int at, bt, ct, wt, tt;
 	int completed;
 	float ntt;
-} p[10];
+} Process;
 
-int n;
+typedef struct Node {
+	struct Node *next;
+	struct Process *p;
+} Node;
 
-void sortByArrival() {
-	struct process temp;
-	int i, j;
+typedef struct Queue {
+	struct Node *front;
+	struct Node *rear;
+} Queue;
 
-	for (i = 0; i < n - 1; i++) {
-		for (j = i + 1; j < n; j++) {
+void sortByArrival(struct Queue *queue) {
+	struct Node *cur_node = queue->front;
+	struct Node *temp;
+	struct Node *prev_node = NULL;
 
-			if (p[i].at > p[j].at) {
+	while (cur_node->next != NULL) {
+		if (cur_node->p->at > cur_node->next->p->at) {
+			temp = cur_node;
 
-				temp = p[i];
-				p[i] = p[j];
-				p[j] = temp;
+			if (prev_node != NULL) {
+				prev_node->next = cur_node->next;
 			}
+			
+			cur_node->next = cur_node->next->next;
+			prev_node->next->next = cur_node;
+			cur_node = prev_node->next;
 		}
+		prev_node = cur_node;
+		cur_node = cur_node->next;
 	}
 }
 
+void enqueue(struct Queue *queue, struct Node *new_node) {
+	if (queue->front == NULL || queue->rear == NULL) {
+		queue->front = new_node;
+		queue->rear = new_node;
+	}
+	else {
+		queue->rear->next = new_node;
+		queue->rear = new_node;
+	}
+}
+
+struct Process *getNodeProcess(struct Queue *queue, int loc) {
+	struct Node *cur_node = queue->front;
+	for (int i = 0; i < loc; i++) {
+		if (cur_node != NULL) {
+			cur_node = cur_node->next;
+		}
+	}
+	return cur_node->p;
+}
+
 void main() {
-	int i, j, t, sum_bt = 0;
+	int sum_bt = 0;
 	char c;
 	float avgwt = 0, avgtt = 0;
-	n = 5;
+	int n = 5;
+	
+	struct Queue *hrrn_queue = malloc(sizeof(struct Queue));
 
 	int arriv[] = { 0, 2, 4, 6, 8 };
 	int burst[] = { 3, 6, 4, 5, 2 };
 
-	for (i = 0, c = 'A'; i < n; i++, c++) {
-		p[i].name = c;
-		p[i].at = arriv[i];
-		p[i].bt = burst[i];
-		p[i].completed = 0;
+	for (int i = 0, c = 'A'; i < n; i++, c++) {
+		struct Node *new_node = malloc(sizeof(struct Node));
+		struct Process *new_process = malloc(sizeof(struct Process));
+		new_node->p = new_process;
 
-		sum_bt += p[i].bt;
+		new_node->p->name = c;
+		new_node->p->at = arriv[i];
+		new_node->p->bt = burst[i];
+		new_node->p->completed = 0;
+		sum_bt += new_node->p->bt;
+
+		enqueue(hrrn_queue, new_node);
 	}
-
-	sortByArrival();
-	printf("\nName\tArrival Time\tBurst Time\wWating Time");
+	sortByArrival(hrrn_queue);
+	printf("\nName\tArrival Time\tBurst Time\tWating Time");
 	printf("\tTurnAround Time\t Normalized TT");
-	for (t = p[0].at; t < sum_bt;) {
+	for (int t = hrrn_queue->front->p->at; t < sum_bt;) {
 		float hrr = -9999;
 		float temp;
 		int loc;
-		for (i = 0; i < n; i++) {
-			if (p[i].at <= t && p[i].completed != 1) {
-				temp = (p[i].bt + (t - p[i].at)) / p[i].bt;
+		int i = 0;
+		struct Node *cur_node = hrrn_queue->front;
+		while (cur_node != NULL) {
+			if (cur_node->p->at <= t && cur_node->p->completed != 1) {
+				temp = (cur_node->p->bt + (t - cur_node->p->at)) / cur_node->p->bt;
 
 				if (hrr < temp) {
 					hrr = temp;
@@ -63,22 +106,25 @@ void main() {
 					loc = i;
 				}
 			}
+			cur_node = cur_node->next;
+			i++;
 		}
+		printf("\nloc: %d", loc);	
+		struct Process *target = getNodeProcess(hrrn_queue, loc);
+		t += target->bt;
+		target->wt = t - target->at - target->bt;
+		target->tt = t - target->at;
 
-		t += p[loc].bt;
-		p[loc].wt = t - p[loc].at - p[loc].bt;
-		p[loc].tt = t - p[loc].at;
+		avgtt += target->tt;
 
-		avgtt += p[loc].tt;
+		target->ntt = ((float)target->tt / target->bt);
 
-		p[loc].ntt = ((float)p[loc].tt / p[loc].bt);
+		target->completed = 1;
 
-		p[loc].completed = 1;
-
-		avgwt += p[loc].wt;
-		printf("\n%c\t\t%d\t\t", p[loc].name, p[loc].at);
-		printf("%d\t\t%d\t\t", p[loc].bt, p[loc].wt);
-		printf("%d\t\t%f", p[loc].tt, p[loc].ntt);
+		avgwt += target->wt;
+		printf("\n%c\t\t%d\t\t", target->name, target->at);
+		printf("%d\t\t%d\t\t", target->bt, target->wt);
+		printf("%d\t\t%f", target->tt, target->ntt);
 	}
 	printf("\nAverage waiting time:%f\n", avgwt / n);
 	printf("Average Turn Around time:%f\n", avgtt / n);
