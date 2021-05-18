41457c68757bb2758c8f53390e04eb316f81dfd5
Ochodus <mario1470@naver.com>
2021-05-18 20:38:17 +0900

    Update directory and Makefile

---
+++ hrrn_include/hrrn_process.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Process {
+
+	int p_num; //process id
+	int at, bt, wt, tt; //arrival time, burst time, wating time, turn-around time
+	int completed;
+	float ntt; //normalized turn-around time
+
+} Process;
+
+struct Process *createProcess(int id, int at, int bt);
+void updateProcessInfo(struct Process *p, int cur_time);
+
---
+++ hrrn_main/hrrn.c
@@ -0,0 +1,37 @@
+#include "hrrn_queue.h"
+#include "scheduling_io.h"
+
+int main() {
+
+	struct Queue *hrrn_queue = malloc(sizeof(struct Queue)); //Creating queue contain processes
+	
+	//Return 1 and quit if failed to load text file
+	if (parseProcess(hrrn_queue)) {
+		return 1;
+	}
+
+	sortByArrival(hrrn_queue);
+
+	printf("\nId\tArrival Time\tBurst Time\tWating Time");
+	printf("\tTurnAround Time\t Normalized TT");
+
+	while (hrrn_queue->size > 0) {
+		
+		struct Process *target = moveCursor(hrrn_queue); //Getting hightest response ratio process
+
+		hrrn_queue->cur_time += target->bt; //Updating current time;
+		
+		updateProcessInfo(target, hrrn_queue->cur_time);
+
+		hrrn_queue->avgtt += target->tt; //Adding turn around time of process to total turn around time
+		hrrn_queue->avgwt += target->wt; //Adding waiting time of process to total waiting time
+
+		printBursted(target);
+		dequeue(hrrn_queue);
+	}
+
+	printf("\nAverage waiting time:%f\n", hrrn_queue->avgwt / hrrn_queue->max_size);
+	printf("Average Turn Around time:%f\n", hrrn_queue->avgtt / hrrn_queue->max_size);
+
+	return 0;
+}
